2aaf64d8d5c4fdd8be70ffbdbd578e6eea4f37a1
Rafael Salgado <rafael.salgado31@gmail.com>
2021-06-16 17:49:10 -0300

    Preparando Ambiente

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio1/RafaelDeSousaSalgado/Exemplo.java
@@ -0,0 +1,7 @@
+package uff.ic.lleme.tcc00328.s20211.exercicio.exercicio1.RafaelDeSousaSalgado;
+
+public class Exemplo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
