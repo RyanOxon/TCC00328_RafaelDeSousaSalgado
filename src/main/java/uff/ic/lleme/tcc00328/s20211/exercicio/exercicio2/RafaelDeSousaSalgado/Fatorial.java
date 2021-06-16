721f855f430eaf33747b93081fba401c1734047c
Rafael Salgado <rafael.salgado31@gmail.com>
2021-06-16 17:52:46 -0300

    Create Fatorial.java

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio2/RafaelDeSousaSalgado/Fatorial.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uff.ic.lleme.tcc00328.s20211.exercicio.exercicio2.RafaelDeSousaSalgado;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Fatorial {
+    
+}
