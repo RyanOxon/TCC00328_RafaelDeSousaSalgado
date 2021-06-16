e0288bff8aa63d19a2c79eca9d44ec5798bbe641
Rafael Salgado <57259396+RyanOxon@users.noreply.github.com>
2021-06-16 17:53:48 -0300

    Update Fatorial.java

--- src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio2/RafaelDeSousaSalgado/Fatorial.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/exercicio2/RafaelDeSousaSalgado/Fatorial.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uff.ic.lleme.tcc00328.s20211.exercicio.exercicio2.RafaelDeSousaSalgado;
 
-/**
- *
- * @author Rafael
- */
+import java.util.Scanner;
+
 public class Fatorial {
-    
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pronto para calcular o fatorial de N");
+        System.out.println("Digite o valor de N:");
+        int inicial = scan.nextInt();
+        int n = inicial;
+        int total = n;
+        do{
+            total *= (n-1);
+            n--;
+        }while(n>1);
+        System.out.println("O fatorial de "+inicial+" é igual a "+ total);
+        scan.close();
+    }
 }
