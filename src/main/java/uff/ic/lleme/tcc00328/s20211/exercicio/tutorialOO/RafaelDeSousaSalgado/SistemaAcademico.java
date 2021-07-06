34d45420528bac6b3a0042dd94b3556ab54e15cc
Rafael Salgado <rafael.salgado31@gmail.com>
2021-07-06 17:17:14 -0300

    TutorialOO v0.2

--- src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/tutorialOO/RafaelDeSousaSalgado/SistemaAcademico.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/tutorialOO/RafaelDeSousaSalgado/SistemaAcademico.java
@@ -3,6 +3,7 @@ package uff.ic.lleme.tcc00328.s20211.exercicio.tutorialOO.RafaelDeSousaSalgado;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class SistemaAcademico {
     private static void main(String[] args){
@@ -11,17 +12,36 @@ public class SistemaAcademico {
         String arqInscricoes = "Inscricoes.txt";
         
         try {
-            String[] alunos = carregarAlunos(arqAluno);
+            Aluno[] alunos = carregarAlunos(arqAluno);
         } catch (FileNotFoundException ex) {
             System.out.println("Erro ao carregar alunos, Arquivo nao encontrado");
+        } catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Numero maximo de alunos atingido, Somente permitido 20 alunos");
         }
         String[] disciplinas = carregarDisciplinas(arqDisciplinas);
         String[] inscricoes = carregarInscricoes(arqInscricoes);
     }
     
-    private static String[] carregarAlunos(String nomeArquivo) throws FileNotFoundException{
+    private static Aluno[] carregarAlunos(String nomeArquivo) throws FileNotFoundException, ArrayIndexOutOfBoundsException{
         InputStream input = new FileInputStream(nomeArquivo);
-        
+        Scanner in = new Scanner(input);
+        String leitura;
+        String[] dados = new String[4];
+        Aluno[] al = new Aluno[20];
+        int pos = 0, posVetor = 0;
+        while(in.hasNext()){
+            leitura = in.next();
+            while(!leitura.equals("\n")){
+                if(pos>3){
+                    dados[pos] = dados[pos].concat(leitura); 
+                }
+                dados[pos] = leitura;
+                pos++;    
+            }
+            al[posVetor] = new Aluno(dados[0], dados[1], dados[2], dados[3]);
+            
+            
+        }
     }
     
     private static String[] carregarDisciplinas(String nomeArquivo){
