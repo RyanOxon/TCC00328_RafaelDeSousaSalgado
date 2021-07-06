fddb86d95aaca00c562bd57de54923f9a8ee004d
Rafael Salgado <rafael.salgado31@gmail.com>
2021-07-06 18:07:13 -0300

    TutorialOO V0.4.2
    carregarInscricoes iniciado.
    Notado possíveis exceptions na leitura do arquivo e iniciado processo para corrigi-las

--- src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/tutorialOO/RafaelDeSousaSalgado/SistemaAcademico.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/tutorialOO/RafaelDeSousaSalgado/SistemaAcademico.java
@@ -22,7 +22,13 @@ public class SistemaAcademico {
         } catch (FileNotFoundException ex) {
             System.out.println("Erro ao carregar Disciplinas, Arquivo nao encontrado");
         }
-        String[] inscricoes = carregarInscricoes(arqInscricoes);
+        try {
+            ArrayList<Inscricao> Inscricoes = carregarInscricoes(arqInscricoes);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Erro ao carregar as Inscricoes, Arquivo nao encontrado");
+        } catch (Exception e) {
+            
+        }
     }
     
     private static ArrayList<Aluno> carregarAlunos(String nomeArquivo) throws FileNotFoundException {
@@ -56,12 +62,17 @@ public class SistemaAcademico {
         int pos = 0;
         while(in.hasNext()){
             leitura = in.next();
-            while(!leitura.equals("\n")){
+            while(!leitura.equals("\n")|| in.hasNext()){
                 if(pos>2){
-                    dados[pos] = dados[pos].concat(leitura); 
-                }
+                    dados[pos] = dados[pos].concat(leitura);
+                    if(in.hasNext()) 
+                        leitura = in.next();
+                }else{
                 dados[pos] = leitura;
-                pos++;    
+                pos++;
+                if(in.hasNext()) 
+                    leitura = in.next();
+                }
             }
             dis.add(new Disciplina(dados[0], dados[1], dados[2]));
             pos = 0;
@@ -69,7 +80,22 @@ public class SistemaAcademico {
         return dis;
     }
 
-    private static String[] carregarInscricoes(String nomeArquivo) {
-        #todo
+    private static ArrayList<Inscricao> carregarInscricoes(String nomeArquivo) throws FileNotFoundException, Exception {
+        InputStream input = new FileInputStream(nomeArquivo);
+        Scanner in = new Scanner(input);
+        String leitura = in.next();
+        String[] dados = new String[4];
+        ArrayList<Inscricao> ins = new ArrayList<>();
+        while(in.hasNext()){
+            
+            for(int i = 0; i<3;i++){
+                dados[i] = leitura;
+                if(in.hasNext()) 
+                    leitura = in.next();
+                else 
+                }
+            
+        }
+        return ins;
     }
 }
