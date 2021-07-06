10dd08246e42d948f3c8fde3900332d33b169073
Rafael Salgado <rafael.salgado31@gmail.com>
2021-07-06 17:34:46 -0300

    TutorialOO V0.3
    carregarAluno pronto usando ArrayList no lugar da array convencional para poder ser redimensionado caso necessário

--- src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/tutorialOO/RafaelDeSousaSalgado/SistemaAcademico.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20211/exercicio/tutorialOO/RafaelDeSousaSalgado/SistemaAcademico.java
@@ -3,6 +3,7 @@ package uff.ic.lleme.tcc00328.s20211.exercicio.tutorialOO.RafaelDeSousaSalgado;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SistemaAcademico {
@@ -12,23 +13,21 @@ public class SistemaAcademico {
         String arqInscricoes = "Inscricoes.txt";
         
         try {
-            Aluno[] alunos = carregarAlunos(arqAluno);
+            ArrayList<Aluno> alunos = carregarAlunos(arqAluno);
         } catch (FileNotFoundException ex) {
             System.out.println("Erro ao carregar alunos, Arquivo nao encontrado");
-        } catch (ArrayIndexOutOfBoundsException ex){
-            System.out.println("Numero maximo de alunos atingido, Somente permitido 20 alunos");
         }
         String[] disciplinas = carregarDisciplinas(arqDisciplinas);
         String[] inscricoes = carregarInscricoes(arqInscricoes);
     }
     
-    private static Aluno[] carregarAlunos(String nomeArquivo) throws FileNotFoundException, ArrayIndexOutOfBoundsException{
+    private static ArrayList<Aluno> carregarAlunos(String nomeArquivo) throws FileNotFoundException {
         InputStream input = new FileInputStream(nomeArquivo);
         Scanner in = new Scanner(input);
         String leitura;
         String[] dados = new String[4];
-        Aluno[] al = new Aluno[20];
-        int pos = 0, posVetor = 0;
+        ArrayList<Aluno> al = new ArrayList<>();
+        int pos = 0;
         while(in.hasNext()){
             leitura = in.next();
             while(!leitura.equals("\n")){
@@ -38,10 +37,10 @@ public class SistemaAcademico {
                 dados[pos] = leitura;
                 pos++;    
             }
-            al[posVetor] = new Aluno(dados[0], dados[1], dados[2], dados[3]);
-            
-            
+            al.add(new Aluno(dados[0], dados[1], dados[2], dados[3]));
+            pos = 0;
         }
+        return al;
     }
     
     private static String[] carregarDisciplinas(String nomeArquivo){
